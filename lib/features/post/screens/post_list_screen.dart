999c77c67fe99ba3ecb61968e4719897e8f70e55
eunchan <whdmscks11@gmail.com>
2025-09-17 17:28:18 +0900

    refactor: HomeScreen과 이름 충돌을 피하기 위해 PostListScreen으로 변경

--- lib/features/post/screens/post_list_screen.dart
+++ lib/features/post/screens/post_list_screen.dart
@@ -4,8 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// 앱 메인 화면 - 로그인 이후 화면 (게시글 목록 화면이 될 가능성이 큼)
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class PostListScreen extends StatelessWidget {
+  const PostListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
