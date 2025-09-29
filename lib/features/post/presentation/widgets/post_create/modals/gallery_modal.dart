74dc5d1f406b9f70303b2f0f1a1750ba6f10f795
eunchan <whdmscks11@gmail.com>
2025-09-29 19:37:33 +0900

    feat: 갤러리 모달에서 사진 불러오기 및 카메라 기능 구현
    -  AppBar에 'Gallery' 타이틀과 'Done' 버튼 추가
    - PhotoManager 의존성 추가 및 권한 요청 처리
    - GalleryModal 실행 시 기기의 첫 번째 앨범(Recent)의 모든 사진 불러옴
    - GridView로 갤러리 표시 (한 줄 3칸, spacing 적용)
    - 첫 번째 칸에 카메라 아이콘 배치 및 카메라 촬영 기능 연결
    - 사진 촬영 후 리스트 갱신하여 갤러리에 즉시 반영

--- lib/features/post/presentation/widgets/post_create/modals/gallery_modal.dart
+++ lib/features/post/presentation/widgets/post_create/modals/gallery_modal.dart
@@ -1,4 +1,8 @@
+import 'package:clozii/core/constants/app_constants.dart';
+import 'package:clozii/core/theme/context_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class GalleryModal extends StatefulWidget {
   const GalleryModal({super.key});
@@ -8,16 +12,100 @@ class GalleryModal extends StatefulWidget {
 }
 
 class _GalleryModalState extends State<GalleryModal> {
+  List<AssetEntity> images = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImages();
+  }
+
+  void _loadImages() async {
+    final permission = await PhotoManager.requestPermissionExtend();
+
+    if (permission.isAuth) {
+      final albums = await PhotoManager.getAssetPathList(
+        type: RequestType.image,
+      );
+
+      if (albums.isNotEmpty) {
+        for (var album in albums) {
+          debugPrint(album.name);
+        }
+
+        final recentAlbum = albums.first;
+        final assetCount = await recentAlbum.assetCountAsync;
+
+        final images = await recentAlbum.getAssetListRange(
+          start: 0,
+          end: assetCount,
+        );
+        debugPrint('images: ${images.length}');
+
+        setState(() => this.images = images);
+      }
+    } else {
+      PhotoManager.openSetting();
+    }
+  }
+
+  void _openCamera() async {
+    debugPrint('Camera tapped');
+    final rawImage = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (rawImage != null) {
+      _loadImages();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        shape: Border(bottom: BorderSide(color: AppColors.black12)),
         leading: IconButton(
           onPressed: () => Navigator.of(context).pop(),
           icon: Icon(Icons.close),
         ),
+        title: Text('Gallery'),
+        actions: [TextButton(onPressed: () {  }, child: Text('Done'))],
       ),
-      body: SingleChildScrollView(child: Column(children: [Text('Gallery')])),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+        ),
+        itemCount: images.length + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return GestureDetector(
+              onTap: _openCamera,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.camera_alt, size: 40.0),
+                  Text('Camera', style: context.textTheme.bodyMedium),
+                ],
+              ),
+            );
+          } else {
+            // 갤러리 사진
+            return FutureBuilder<Widget>(
+              future: images[index - 1]
+                  .thumbnailDataWithSize(ThumbnailSize(200, 200))
+                  .then((data) => Image.memory(data!, fit: BoxFit.cover)),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData) {
+                  return GestureDetector(onTap: () {debugPrint('image tapped');}, child: snapshot.data!);
+                }
+                return Container(color: Colors.red[200]);
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
