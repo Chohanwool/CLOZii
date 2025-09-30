f82fc8d38b575759f11e0f1e03c9f3b4331afadd
Chohanwool <hanwoolc95@gmail.com>
2025-09-30 22:44:19 +0900

    feat-auth: Domain - repository 작성

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,30 @@
+import 'package:clozii/features/auth/domain/entities/auth_result.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthRepository {
+  // 회원가입
+  Future<AuthResult<User>> signUp({
+    required String name,
+    required String phoneNumber,
+    required DateTime birthDate,
+    required String gender,
+  });
+
+  // 로그인
+  Future<AuthResult<User>> signIn(String phoneNumber);
+
+  // 로그아웃
+  Future<AuthResult<User>> signOut();
+
+  // 전화번호 중복 확인
+  Future<AuthResult<bool>> isPhoneRegistered(String phoneNumber);
+
+  // 인증번호 전송
+  Future<AuthResult<bool>> sendVerificationCode(String phoneNumber);
+
+  // 인증번호 재전송
+  Future<AuthResult<bool>> resendVerificationCode(String phoneNumber);
+
+  // 인증번호 검증
+  Future<AuthResult<bool>> verifyCode(String phoneNumber, String code);
+}
