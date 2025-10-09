4220c7ac0de3f9bfd883360263030c74b431b20a
조은찬 <whdmscks11@gmail.com>
2025-10-09 22:48:16 +0900

    feat: MeetingPointSelector 위젯 세부 구현
    - GoogleMap 위젯을 활용하여 지도 표시
    - Stack을 사용해 화면 중앙에 마커 고정
    - onCameraIdle에서 중앙 마커의 실제 좌표를 계산해 centerLatLng에 저장

--- lib/features/post/presentation/widgets/post_create/meeting_point_selector.dart
+++ lib/features/post/presentation/widgets/post_create/meeting_point_selector.dart
@@ -1,25 +1,81 @@
 // core
-import 'package:clozii/core/constants/app_constants.dart';
-import 'package:clozii/core/theme/context_extension.dart';
-
 // package
+import 'package:clozii/core/theme/context_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MeetingPointSelector extends StatelessWidget {
+class MeetingPointSelector extends StatefulWidget {
   const MeetingPointSelector({super.key});
 
   @override
+  State<MeetingPointSelector> createState() => _MeetingPointSelectorState();
+}
+
+class _MeetingPointSelectorState extends State<MeetingPointSelector> {
+  late final GoogleMapController? _controller;
+  LatLng centerLatLng = LatLng(14.2639, 121.0742);
+
+  static const CameraPosition kInitialPosition = CameraPosition(
+    target: LatLng(14.2639, 121.0742),
+    zoom: 17.0,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        color: AppColors.black,
-        child: Center(
-          child: Text(
-            'Google Map',
-            style: context.textTheme.titleLarge!.copyWith(color: Colors.white),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: kInitialPosition,
+            onMapCreated: (controller) {
+              _controller = controller;
+            },
+            onCameraIdle: () async {
+              if (_controller == null) return;
+
+              // 화면 크기 (픽셀 단위)
+              final screenWidth = MediaQuery.of(context).size.width;
+              final screenHeight = MediaQuery.of(context).size.height;
+
+              // 중앙 좌표
+              final centerX = (screenWidth / 2).round();
+              final centerY = (screenHeight / 2).round();
+
+              // 실제로 마커가 가리키는 좌표
+              // centerX, centerY 만으로는 아래의 마커가 가르키는 지점과 일치하지 않음 따라서 centerY에 135를 빼줌
+              LatLng markerLatLng = await _controller.getLatLng(
+                ScreenCoordinate(x: centerX, y: centerY - 135),
+              );
+
+              setState(() {
+                centerLatLng = markerLatLng;
+              });
+
+              debugPrint("마커가 가리키는 지도 좌표: $markerLatLng");
+            },
+            // markers: {
+            //   Marker(
+            //     markerId: MarkerId('center_marker'),
+            //     position: centerLatLng,
+            //   ),
+            // },
           ),
-        ),
+
+          Center(
+            child: IgnorePointer(
+              ignoring: true,
+              child: Transform.translate(
+                offset: const Offset(0, -20), // 살짝 위/아래로 조정 가능
+                child: Icon(
+                  Icons.place,
+                  size: 60,
+                  color: context.colors.primary,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
