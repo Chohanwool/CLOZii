539000a84c70648aa75ee0d6bd9914e6419620c7
조은찬 <whdmscks11@gmail.com>
2025-11-10 17:01:57 +0900

    feat: 지역 선택 드롭다운을 한국 도시에서 필리핀 도시로 변경
    - _selectedRegion 초기값을 '서울특별시'에서 'Sta.Rosa'로 변경
    - _regions 리스트를 필리핀 주요 도시 목록으로 교체
    - 검색 아이콘을 CupertinoIcons.search로 통일

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -20,7 +20,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
-  String _selectedRegion = '서울특별시';
+  String _selectedRegion = 'Sta.Rosa';
   bool _isDropdownOpen = false;
 
   // 각 탭에 해당하는 화면들
@@ -33,23 +33,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
   // 지역 목록
   final List<String> _regions = [
-    '서울특별시',
-    '부산광역시',
-    '대구광역시',
-    '인천광역시',
-    '광주광역시',
-    '대전광역시',
-    '울산광역시',
-    '세종특별자치시',
-    '경기도',
-    '강원도',
-    '충청북도',
-    '충청남도',
-    '전라북도',
-    '전라남도',
-    '경상북도',
-    '경상남도',
-    '제주특별자치도',
+    'Sta.Rosa',
+    'Calamba',
+    'Cabuyao',
+    'Tagaytay',
+    'Makati',
+    'Alabang',
+    'Binan',
+    'Quezon City',
+    'Mandaluyong',
+    'Parañaque',
+    'Pasay',
+    'Pasig',
+    'San Juan',
+    'Taguig',
+    'Valenzuela',
   ];
 
   final List<Widget> _filterButtons = [
@@ -117,7 +115,7 @@ class _HomeScreenState extends State<HomeScreen> {
             actions: [
               IconButton(
                 onPressed: () {},
-                icon: const Icon(Icons.search),
+                icon: const Icon(CupertinoIcons.search),
                 iconSize: 28,
               ),
               IconButton(
