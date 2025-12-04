73a61c7e93b5c0b48ee63c766599d53613729c86
조은찬 <whdmscks11@gmail.com>
2025-12-04 18:33:50 +0900

    feat: Post 엔티티를 위한 PostStatus 및 Category enum 추가
    - PostStatus enum 생성 (selling, reserved, soldOut, deleted)
    - Category enum 생성 (20개 카테고리: 수입명품, 패션의류, 가전제품 등)
    - Post 엔티티에 거래 상태 및 카테고리 필드 추가를 위한 기반 작업

---
+++ lib/features/post/core/enums/post_category.dart
@@ -0,0 +1,23 @@
+// 게시글 카테고리를 나타내는 열거형
+enum PostCategory {
+  luxuryGoods, // 수입명품
+  ticketsAndCoupons, // 티켓/쿠폰
+  freeGiveaway, // 무료나눔
+  fashionApparel, // 패션의류
+  fashionAccessories, // 패션잡화
+  beauty, // 뷰티
+  babyAndMaternity, // 출산/유아 용품
+  mobileAndTablets, // 모바일/태블릿
+  homeAppliances, // 가전제품
+  laptopsAndPcs, // 노트북/PC
+  camerasAndCamcorders, // 카메라/캠코더
+  furnitureAndInterior, // 가구/인테리어
+  homeAndLiving, // 리빙/생활
+  videoGames, // 게임
+  petsAndHobbies, // 반려동물/취미
+  booksAndMusic, // 도서/음반/취미
+  sports, // 스포츠
+  leisureAndTravel, // 레저/여행
+  carsAndMotorcycles, // 자동차/오토바이
+  toolsAndIndustrial, // 공구/산업용품
+}
---
+++ lib/features/post/core/enums/post_status.dart
@@ -0,0 +1,7 @@
+// 게시글 상태를 나타내는 열거형
+enum PostStatus {
+  selling, // 판매중
+  reserved, // 예약중
+  soldOut, // 거래완료
+  deleted, // 삭제됨
+}
